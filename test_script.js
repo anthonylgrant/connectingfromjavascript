cfd047097f96ce1e011cfff08b43a426b85f884a
anthonylgrant <anthonylorenzogrant@gmail.com>
2016-11-23 04:52:26 +0000

    defined migration and milestones table, with proper column names

--- test_script.js
+++ test_script.js
@@ -1,6 +1,7 @@
 const pg = require("pg");
 const settings = require("./settings"); //settings.json
 const input = process.argv[2];
+
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
